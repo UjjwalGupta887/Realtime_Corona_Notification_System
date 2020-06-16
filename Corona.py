78df37024d3e9271a0b0d1560d5d813c31ecfee0
Ujjwal Gupta <ujjwalg887@gmail.com>
2020-06-16 14:21:50 +0530

    Corona Virus Notification

---
+++ Corona.py
@@ -0,0 +1,40 @@
+from plyer import notification
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+
+def notify_Me(title,message):
+    notification.notify(title=title,
+                        message=message,
+                        app_icon="C:\\Users\\DELL\\Downloads\\corona(1).ico",
+                        timeout=5)
+
+def getdata(url):
+    r=requests.get(url)
+    return r.text
+
+if __name__ == '__main__':
+    notify_Me('Ujjwal', 'Lets stop the spread of this virus together.')
+    while True:
+        my_html_data = getdata('https://www.mohfw.gov.in/')
+
+        soup = BeautifulSoup(my_html_data, 'html.parser')
+        # print(soup.prettify())
+
+        mydatastr = ''
+        for tr in soup.find_all('tbody')[0].find_all('tr'):
+            mydatastr += tr.get_text()
+
+        mydatastr = mydatastr[1:]
+        itemlist = mydatastr.split('\n\n')
+
+        states = ['Chandigarh', 'Telangana', 'Uttar Pradesh', 'Delhi', 'Maharashtra', 'Punjab']
+        for item in itemlist[0:35]:
+            datalist = (item.split('\n'))
+            if datalist[1] in states:
+                print(datalist)
+                ntitle = 'Cases of covid-19'
+                ntext = f"{datalist[1]}: Active cases: {datalist[2]}\n Cured/Discharged/Migrated : {datalist[3]}\n deaths : {datalist[4]}\n total Cases : {datalist[5]}\n"
+                notify_Me(ntitle, ntext)
+                sleep(2)
+        sleep(3600) #for every hour
